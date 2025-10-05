9ffaa732a1315772c874a7b846677adc3e460599
Hasmat <mdhasmat2023@gmail.com>
2025-10-05 20:49:44 +0530

    complete counter

--- firstPhase/src/App.jsx
+++ firstPhase/src/App.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import './App.css'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 
 const App = () => {
+
+  const [counter, setCounter] = useState(5)
+
+  const addValue = () => {
+      setCounter(prevCounter => prevCounter +1)
+      setCounter(prevCounter => prevCounter +1)
+      setCounter(prevCounter => prevCounter +1)
+      setCounter(prevCounter => prevCounter +1)
+    }
+
+  const removeValue = () => {
+    if (counter > -5) {
+      setCounter(counter -1)
+    }
+  }
+
   return (
-    <div className='text-white text-3xl font-bold underline'>
-      hellow world
-    </div>
+    <>
+      <h1>My Counter App</h1>
+      <h2>Counter value : {} </h2>
+
+      <button onClick={addValue}>Add value {counter}</button>
+      <br />
+      <button onClick={removeValue}>remove value {counter}</button>
+      <p>footer: {counter}</p>
+    </>
   )
 }
 
